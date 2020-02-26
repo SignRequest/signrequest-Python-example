683c7588120f5fb75e1a81cdc8a6740fb5ae6a39
Lucas Pascholatti <lucaspascholatti@Lucass-MacBook-Air.local>
2020-02-26 16:47:26 +0100

    Add a createdocs.py that allows users to post new docs to SignRequest

---
+++ createdocs.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+response = requests.post(
+    "https://yourteam.signrequest.com/api/v1/documents/",
+    headers={"Authorization": "Token YOUR_TEAM_TOKEN_HERE"},
+    data={
+        "file_from_url": 'your_document_url_here',
+        "auto_delete_days": 1
+        # Add other parameters here
+    }
+)
+
+json_response = json.dumps(response.json(), indent=4)
+
+print("Status:", response.status_code, "\n")
+if response.status_code == 201:
+    print("Success! Document Created!" + "\n")
+elif response.status_code == 404:
+    print("Not found.")
+print("Response: ", str(json_response))
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # SignRequest API - Python Client - Lite
 
-Client under development to make GET requests to SignRequest's API
+Client developed to make GET and POST requests to SignRequest's API.
+
+## getdocuments.py
+
+On this file it is possible to make GET requests to ```/documents/``` using your team's credentials.
+
+## createdocs.py
+
+On this file it is possible to send POST requests to ```/documents/``` so that you can send sign requests with it. 
