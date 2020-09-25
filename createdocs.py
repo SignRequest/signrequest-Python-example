2f64e870c45685657c9d39a9ac676f9967cb6e8b
Lucas Pascholatti <lucaspascholatti@Lucass-MacBook-Air.local>
2020-09-25 15:49:04 +0200

    Commiting file_from_content example and encode to base64

--- createdocs.py
+++ createdocs.py
@@ -2,11 +2,23 @@ import requests
 import json
 import base64
 
+PDF_NAME = "demo_document.pdf"
+
+pdf = open("demo_document.pdf", "rb").read()
+encodedPdf = base64.b64encode(pdf)
+
+#print("Original PDF: ", pdf)
+#print("Encoded PDF: ", encodedPdf)
+
 response = requests.post(
     "https://yourteam.signrequest.com/api/v1/documents/",
     headers={"Authorization": "Token YOUR_TOKEN_HERE"},
     data={
-        "file_from_url": 'your_url_here',
+        # "file_from_url" : "your_url",
+        # do not use file_from_url simultaneously with file_from_content
+        "file_from_content": encodedPdf,
+        "file_from_content_name": PDF_NAME,
+        "name": PDF_NAME,
         "auto_delete_days": 1
         # Add other parameters here
     }
--- quickcreatesr.py
+++ quickcreatesr.py
@@ -1,20 +1,35 @@
 import requests
 import json
+import base64
 
-data = {
-    "file_from_url": "your_file_url_here",
-    "signers": [
-        {
-            "email": "name+1@provider.com"
-        },
-        {
-            "email": "name+2@provider.com"
-        }
-    ],
+UTF8 = "utf-8"
+PDF_NAME = "demo_document.pdf"
+
+# first: reading the binary stuff
+# note the 'rb' flag
+# result: bytes
+with open("demo_document.pdf", "rb") as pdf_content:
+    pdf_byte_content = pdf_content.read()
+
+# second: base64 encode read data
+# result: bytes
+encoded_pdf = base64.b64encode(pdf_byte_content)
+
+# third: decode these bytes to text
+# result: string (in utf-8)
+pdf_base64_string = encoded_pdf.decode(UTF8)
+
+json_data = {
+    # "file_from_url" : "your_url", 
+    # do not use file_from_url simultaneously with file_from_content
+    "signers": [{"email": "name@provider.com", "order": 0}, {"email": "name+1@provider.com", "order": 1}],
+    "file_from_content": pdf_base64_string,
+    "file_from_content_name": PDF_NAME,
+    "name": PDF_NAME,
     "from_email": "name@provider.com",
     "message": "Please sign this document - Test",
     "subject": "SignTest - YourTeam API - Python",
-    "who": "o",
+    "who": "mo",
     "auto_delete_days": 1
     # Add other parameters here
 }
@@ -22,10 +37,10 @@ data = {
 response = requests.post(
     "https://yourteam.signrequest.com/api/v1/signrequest-quick-create/",
     headers={"Authorization": "Token YOUR_TOKEN_HERE"},
-    json=data
+    json=json_data
 )
 
-print("Data:", data, "\n")
+print("Data:", json_data, "\n")
 
 json_response = json.dumps(response.json(), indent=4)
 
